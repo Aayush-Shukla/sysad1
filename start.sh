2c65d3d1e8e2117663b40c2dd8cf922191223607
SHUKLA <hydra@pop-os.localdomain>
2020-04-23 18:00:04 +0530

    added nearest and finalattendance

--- start.sh
+++ start.sh
@@ -8,6 +8,8 @@ echo "alias userGenerate="$m/create.sh"" >> ~/.bashrc
 echo "alias permit="$m/permission.sh"" >> ~/.bashrc 
 echo "alias autoSchedule="$m/location.sh"" >> ~/.bashrc
 echo "alias attendance="$m/updatechief.sh"" >> ~/.bashrc 
+echo "alias finalattendance="$m/finalat.sh"" >> ~/.bashrc 
+echo "alias nearest="$m/near.sh"" >> ~/.bashrc 
 
 
 
