dbd0fd31c4d0235348f019f238ae24e755796db6
SHUKLA <hydra@pop-os.localdomain>
2020-04-26 16:02:32 +0530

    fixed dates

--- finalat.sh
+++ finalat.sh
@@ -1,7 +1,15 @@
- #!/bin/bash
+#!/bin/bash
  
- touch /home/ChiefCommander/attendance_report.txt
+touch /home/ChiefCommander/attendance_report.txt
+
+if [ $(date +"%H") -lt 6 ]
+    then
+    dat=$(date +"%Y-%m-%d")
+    
+    else
+    dat=$(date -d "+1day" +"%Y-%m-%d")
+fi
+    
  
- 
- 
- paste `dirname $0`/sysad-task1-attendance.log | awk '{print substr($1,2,10),$2,$3}' > /home/ChiefCommander/attendance_report.txt
+sed -ne "/2020-04-11/,/$dat/{/$dat/d; p}" sysad-task1-attendance.log | awk '{print substr($1,2,10),$2,$3}'> /home/ChiefCommander/attendance_report.txt
+
